834690db916689bcd192903e8167c797ee9ea14d
Alexey Baranov <atlantij@gmail.com>
2016-12-17 17:18:43 +0300

    upd forum perm

--- machina/apps/forum_permission/checker.py
+++ machina/apps/forum_permission/checker.py
@@ -52,7 +52,8 @@ class ForumPermissionChecker(object):
             elif self.user:
                 default_auth_forum_perms = \
                     machina_settings.DEFAULT_AUTHENTICATED_USER_FORUM_PERMISSIONS
-                if self.user.groups.count():
+                if not self.user.is_anonymous() and (
+                        self.user.groups.exists() or UserForumPermission.objects.filter(user=self.user).exists()):
                     default_auth_forum_perms = []
 
                 user_kwargs_filter = {'anonymous_user': True} if self.user.is_anonymous() \
